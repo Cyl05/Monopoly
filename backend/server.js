23a3a0a03db248c7220fe3c9da694f5ad6d9b65a
Ethan Saju <ethansaju10@gmail.com>
2025-01-30 16:49:29 +0530

    Intial test

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+  res.send("Good Morning");
 });
 
 app.listen(port, () => {
-    console.log("Server started at http://localhost:3000");
+  console.log("Server started at http://localhost:3000");
 });
