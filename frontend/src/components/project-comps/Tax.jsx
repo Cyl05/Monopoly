b0107bc270b784823bcfcb433fbdf4085fa27383
Ethan Saju <ethansaju10@gmail.com>
2025-02-06 23:46:45 +0530

    Tax component and treasuresAndSuprises functions

---
+++ frontend/src/components/project-comps/Tax.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Icon, VStack, Heading } from "@chakra-ui/react";
+import { BiMoneyWithdraw } from "react-icons/bi";
+
+const Tax = (props) => (
+  <Box background="gray">
+    <VStack padding={"1vh 1vw"}>
+      <Icon fontSize="4vh" textAlign={"center"}>
+        <BiMoneyWithdraw />
+      </Icon>
+      <Heading size="sm">{props.type}</Heading>
+    </VStack>
+  </Box>
+);
+
+export default Tax;
